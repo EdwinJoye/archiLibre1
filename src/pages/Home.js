dacd8a9688a0e250e8c61af431baaa6f13dbd3ab
Edwin <edwinjoye@gmail.com>
2023-03-13 13:22:22 +0100

    test2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,23 @@
 import MenuHome from "../components/MenuHome";
 import "../css/home.css";
+import { home } from "../data/home";
 
 const Home = () => {
   return (
     <div>
       <div>Home</div>
-      <MenuHome></MenuHome>
+      <div className="homeDiv">
+        <MenuHome></MenuHome>
+        <div>
+          {home.map((data, key) => {
+            return (
+              <div>
+                <img className="homeImg" src={data.mainUrl} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
