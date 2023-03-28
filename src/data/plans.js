855d32809813125ccbb51e721c1836b3e63e1024
Edwin <edwinjoye@gmail.com>
2023-03-28 21:04:58 +0200

    signatures finies

---
+++ src/data/plans.js
@@ -0,0 +1,41 @@
+export const plans = [
+  {
+    id: "plan1",
+    class: "plan1",
+    title: "plan 1",
+    description: "no description",
+    mainUrl:
+      "https://res.cloudinary.com/dt7srgrd1/image/upload/v1680011012/archilibre1/Photos/Plans/plan2_-_copie_e9qzqw.jpg",
+    zooms: [
+      //   "https://res.cloudinary.com/dm0sv9nfx/image/upload/v1651683912/mozartWorksTattooPictures/5-Colored%20Drawings/ColoredDrawingsSlides/ColoredDrawingsSlide1/1_coshuo.jpg",
+    ],
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+  },
+  {
+    id: "plan1",
+    class: "plan2",
+    title: "plan 2",
+    description: "no description",
+    mainUrl:
+      "https://res.cloudinary.com/dt7srgrd1/image/upload/v1680011012/archilibre1/Photos/Plans/plan1_-_copie_htl0br.jpg",
+    zooms: [
+      //   "https://res.cloudinary.com/dm0sv9nfx/image/upload/v1651683912/mozartWorksTattooPictures/5-Colored%20Drawings/ColoredDrawingsSlides/ColoredDrawingsSlide1/1_coshuo.jpg",
+    ],
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+  },
+  {
+    id: "plan1",
+    class: "plan1",
+    title: "plan 3",
+    description: "no description",
+    mainUrl:
+      "https://res.cloudinary.com/dt7srgrd1/image/upload/v1680011011/archilibre1/Photos/Plans/plan3_-_copie_riom6x.jpg",
+    zooms: [
+      //   "https://res.cloudinary.com/dm0sv9nfx/image/upload/v1651683912/mozartWorksTattooPictures/5-Colored%20Drawings/ColoredDrawingsSlides/ColoredDrawingsSlide1/1_coshuo.jpg",
+    ],
+    texte:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
+  },
+];
--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -1,11 +1,52 @@
 import Header from "../components/Header";
 import "../css/plans.css";
+import { plans } from "../data/plans";
 
 const Plans = ({ onOff, setOnOff }) => {
   return (
     <div className="container">
       <Header onOff={onOff} setOnOff={setOnOff}></Header>
-      <div>Plans</div>
+      <div>
+        {plans.map((data, key) => {
+          if (data.class === "plan1") {
+            return (
+              <div key={key}>
+                <div className="title-plans1">{data.title}</div>
+                <div className="img-texte-plans">
+                  <div className="div-img-plans img-border-1">
+                    <img src={data.mainUrl} alt="pic" />
+                    <div className="imageOverlay">
+                      <div className="imageTitle">{data.title}</div>
+                      <div className="descriptionImage">{data.description}</div>
+                    </div>
+                  </div>
+                  <div className="texte-plans">
+                    <p>{data.texte}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div key={key}>
+                <div className="title-plans2">{data.title}</div>
+                <div className="img-texte-plans">
+                  <div className="texte-plans">
+                    <p>{data.texte}</p>
+                  </div>
+                  <div className="div-img-plans img-border-1">
+                    <img src={data.mainUrl} alt="pic" />
+                    <div className="imageOverlay">
+                      <div className="imageTitle">{data.title}</div>
+                      <div className="descriptionImage">{data.description}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
