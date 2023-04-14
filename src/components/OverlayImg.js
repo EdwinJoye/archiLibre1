a4e53ee271aa11308b0926d600c84e1bead55c56
Edwin <edwinjoye@gmail.com>
2023-04-14 19:54:44 +0200

    overlay peut fermer

--- src/components/OverlayImg.js
+++ src/components/OverlayImg.js
@@ -1,16 +1,20 @@
+// import Croix from "../img/croixBlanche.svg";
+
 import "../css/overlayImg.css";
-import Croix from "../img/croixBlanche.svg";
 
 const OverlayImg = ({ url, isSelected, onClick }) => {
   return (
-    <div className={`invisible ${isSelected ? "visible" : ""}`}>
+    <div
+      onClick={() => onClick()}
+      className={`invisible ${isSelected ? "visible" : ""}`}
+    >
       <div className="overlayImg__box-img">
-        <img
+        {/* <img
           className="croixCarouselOverlay"
           onClick={() => onClick()}
           src={Croix}
           alt="croix"
-        />
+        /> */}
         <img className="overlayImg__img" src={url} alt="" />
       </div>
     </div>
