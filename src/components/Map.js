58d0fe7f5a535b05ed48be070729581e580e43d5
Edwin <edwinjoye@gmail.com>
2023-05-09 12:06:49 +0200

    map faite. Début création formulaire

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,22 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
+import LocationPin from "./LocationPin";
 import "../css/contact.css";
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function SimpleMap() {
+  const location = {
+    address: "Cabinet AOtheninGirard",
+    lat: 48.822430661529026,
+    lng: 2.208630993253485,
+  };
   const defaultProps = {
     center: {
       lat: 48.822430661529026,
       lng: 2.208630993253485,
     },
-    zoom: 11,
+    zoom: 15,
   };
 
   return (
@@ -18,9 +24,14 @@ export default function SimpleMap() {
     <div className="contact__map">
       <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyDw8C1USZ9tllcVvnuQ70GWil8tp3XpETk" }}
-        defaultCenter={defaultProps.center}
+        defaultCenter={location}
         defaultZoom={defaultProps.zoom}
       >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
         <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
       </GoogleMapReact>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,10 +5,24 @@ import "../css/contact.css";
 
 const Contact = ({ onOff, setOnOff }) => {
   return (
-    <div className="relative">
+    <div className="contact__all-test relative">
       <HeaderGreen1 onOff={onOff} setOnOff={setOnOff}></HeaderGreen1>
-      <div className="container"></div>
-      <Map></Map>
+      <div className="contact__all-container container">
+        <div className="contact__container-1">
+          <img
+            src="https://res.cloudinary.com/dt7srgrd1/image/upload/v1683567160/archilibre1/Photos/Aman/my_spa_oim_pensif_gqpaxk.jpg"
+            alt="aman"
+          />
+        </div>
+        <div className="contact__container-2">
+          <div className="contact__infos-container">
+            <p>25 avenue de l’Europe</p>
+            <p>AOtheninGirard@gmail.com</p>
+            <p>06.50.02.85.06</p>
+          </div>
+          <Map></Map>
+        </div>
+      </div>
       <div className="footer">
         <Footer onOff={onOff} setOnOff={setOnOff}></Footer>
       </div>
