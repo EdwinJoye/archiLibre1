ae5304344f0e6a746b77775e64801c8af5a12585
Edwin <edwinjoye@gmail.com>
2023-05-23 17:47:41 +0200

    page contact faite

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,8 @@
 import HeaderGreen1 from "../components/HeaderGreen1";
 import Footer from "../components/Footer";
+import Email from "../img/mail.svg";
+import Position from "../img/position.svg";
+import Telephone from "../img/tel.svg";
 import Map from "../components/Map";
 import "../css/contact.css";
 
@@ -13,17 +16,52 @@ const Contact = ({ onOff, setOnOff }) => {
             src="https://res.cloudinary.com/dt7srgrd1/image/upload/v1683567160/archilibre1/Photos/Aman/my_spa_oim_pensif_gqpaxk.jpg"
             alt="aman"
           />
+          <div className="contact__img-background"></div>
         </div>
         <div className="contact__container-2">
-          <Map></Map>
+          <div className="title__container-all">
+            <div className="title__date-container">
+              <h2 className="txt-vert-c">Contact</h2>
+              <div className="title__underline und-vert-c"></div>
+              <span>A.Othenin Girard</span>
+            </div>
+            <div className={`contact__title-background bg-vert-tc`}></div>
+          </div>
+
           <div className="contact__infos-container">
-            <span>25 avenue de l’Europe</span>
-            <span>AOtheninGirard@gmail.com</span>
-            <span>06.50.02.85.06</span>
-            <a HREF="mailto:AOtheninGirard@gmail.com">Contactez moi</a>
+            <div className="contact__logo-info">
+              <div className="contact__logo-container">
+                <img src={Email} alt="email" />
+              </div>
+              <a
+                className="contact__mail"
+                HREF="mailto:AOtheninGirard@gmail.com"
+              >
+                AOtheninGirard@gmail.com
+              </a>
+            </div>
+            <div className="contact__logo-info">
+              <div className="contact__logo-container">
+                <img src={Telephone} alt="telephone" />
+              </div>
+              <span>06.50.02.85.06</span>
+            </div>
+            <div className="contact__logo-info">
+              <div className="contact__logo-container">
+                <img src={Position} alt="position" />
+              </div>
+              <span>25 avenue de l’Europe</span>
+            </div>
+            <a
+              className="contact__button"
+              HREF="mailto:AOtheninGirard@gmail.com"
+            >
+              Contactez moi
+            </a>
           </div>
         </div>
       </div>
+      <Map></Map>
       <Footer onOff={onOff} setOnOff={setOnOff}></Footer>
     </div>
   );
